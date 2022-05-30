d8aed7a50b3481f599c1e8bead62a31fdcd4f1be
Bener <benjavan@163.com>
2022-05-30 10:49:05 +0800

    fix: add default manager 'npm'

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { lockFileMap, cmdMap, type Action } from './config'
 const cwd = process.cwd()
 const args = process.argv.slice(2).filter(Boolean)
 const action: Action = args[0] === '-' ? (args.shift(), 'uninstall') : 'install'
-let pkgManager: string
 
 export async function tpm() {
   if (!args.length) {
@@ -14,23 +13,23 @@ export async function tpm() {
     return
   }
 
+  const manager = await getManager()
+  const packages = args.map(pkg => `@types/${pkg}`)
+  const act = cmdMap[action].get(manager)
+  const command = `${manager} ${act} ${packages.join(' ')} -D`
+  console.log('manager:', manager)
+  console.log(`${action}:`, args.join(', '))
+  console.log(command)
+  spawn(command, { shell: true, stdio: 'inherit', cwd })
+}
+
+async function getManager() {
+  let manager = 'npm'
   for await (const [manager, lockFile] of lockFileMap) {
     try {
       await fs.access(path.resolve(cwd, lockFile))
-      pkgManager = manager
-      const packages = args.map(pkg => `@types/${pkg}`)
-      const act = cmdMap[action].get(manager)
-      const command = `${manager} ${act} ${packages.join(' ')} -D`
-      console.log('manager:', manager)
-      console.log(`${action}:`, args.join(', '))
-      console.log(command)
-      spawn(command, { shell: true, stdio: 'inherit', cwd })
+      return manager
     } catch (_) {}
-
-    if (pkgManager) return
   }
-  console.error(
-    'Error: No package manager found. Please install a package manager like:',
-    [...lockFileMap.keys()].join(', ')
-  )
+  return manager
 }
